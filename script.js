21e08c4e6334b6105e89e1f5ec00250f9ff23a5a
Alana-Rocha <alanarodrigues.rocha1@gmail.com>
2023-12-04 13:12:00 -0300

    refactor: init

---
+++ script.js
@@ -0,0 +1,26 @@
+function calculateTip(event) {
+  event.preventDefault();
+  let bill = document.getElementById("bill").value;
+  let serviceQual = document.getElementById("serviceQual").value;
+  let numbOfPeople = document.getElementById("people").value;
+
+  if ((bill == " ") || (serviceQual == 0)) {
+    alert("Por favor, preencha os valores");
+    return;
+  }
+
+  if (numbOfPeople == " " || numbOfPeople <= 1) {
+    numbOfPeople = 1;
+    document.getElementById("each").style.display = "none";
+  }
+    document.getElementById("each").style.display = "block";
+
+  let total = (bill * serviceQual) / numbOfPeople;
+  total = total.toFixed(2);
+  document.getElementById("tip").innerHTML = total;
+  document.getElementById("totalTip").style.display = "block";
+}
+
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+document.getElementById("tipsForm").addEventListener("submit", calculateTip);
